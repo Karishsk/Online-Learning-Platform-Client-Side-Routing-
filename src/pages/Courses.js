206ed6eba9548ae8a6ae7a04a5ef5188d5ad8b1a
karishma S K <144902047+Karishsk@users.noreply.github.com>
2024-11-14 10:23:39 +0530

    Add files via upload

---
+++ src/pages/Courses.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Courses() {
+    return <h2>Explore Our Courses</h2>;
+}
+
+export default Courses;
+
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+    return <h2>Welcome to Learnify - Your Online Learning Platform</h2>;
+}
+
+export default Home;
